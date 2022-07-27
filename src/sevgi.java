6b873591e1eb6b82fb632614a941dd28459da104
Sevgiay <fztsevgiayy@gmail.com>
2022-07-27 11:59:04 -1000

    project

--- src/sevgi.java
+++ src/sevgi.java
@@ -2,7 +2,10 @@ public class sevgi {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("addd");
-        //more stuff
-        //Mac user and windows users are happy to committ
+          //more stuff
+         //Mac user and windows users are happy to committ
+        // it s now  i am ready published on GitHub.com
+
+
     }
 }
