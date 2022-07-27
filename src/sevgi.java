02ee0fb3950569d3821c08c2267a4ffd0436527a
Sevgiay <fztsevgiayy@gmail.com>
2022-07-27 11:35:16 -1000

    added new java class called Hello

--- src/sevgi.java
+++ src/sevgi.java
@@ -1,5 +1,8 @@
 public class sevgi {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("addd");
+        //more stuff
+        //Mac user and windows users are happy to committ
     }
 }
